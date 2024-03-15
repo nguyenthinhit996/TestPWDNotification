03a1634d920e1af4934fe415bd3b4f19c6fc009a
Thinh Nguyen <37232471+nguyenthinhit996@users.noreply.github.com>
2024-03-15 07:05:44 +0700

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,4 +1,4 @@
-importScripts(
+ importScripts(
   "https://cdnjs.cloudflare.com/ajax/libs/firebase/10.8.1/firebase-app-compat.min.js"
 );
 importScripts(
@@ -23,70 +23,33 @@ messaging.onBackgroundMessage(function (payload) {
     payload
   );
 
-  // const notificationTitle = payload.notification.title;
-  // const notificationOptions = {
-  //   body: payload.notification.body,
-  //   data: payload.data,
-  //   taskId: payload.data.taskId,
-  //   tag: "notification-1",
-  // };
-
-  self.addEventListener(
-    "notificationclick",
-    function (e) {
-      var t = e.notification;
-      if ((console.log("notificationclick", e), "" !== e.action))
-        "close" === e.action && t.close();
-      else {
-        var n = "http://localhost:3000";
-        n && self.clients.openWindow(n);
-      }
-    },
-    !1
-  );
-
-  self.addEventListener("install", function (e) {
-    self.skipWaiting(), console.log("MagicBellSW:install");
-  });
-  self.addEventListener("activate", function (e) {
-    console.log("MagicBellSW:activate"), e.waitUntil(self.clients.claim());
-  });
-  self.addEventListener("push", function (e) {
-    var t,
-      n = null === (t = e.data) || void 0 === t ? void 0 : t.json(),
-      r = {
-        body: n.content || "",
-        vibrate: [100, 50, 100],
-        data: "data nek",
-        actions: [{ action: "close", title: "Close" }],
-      };
-    e.waitUntil(self.registration.showNotification("n.title", r));
-  });
-
-  const notificationTitle = "Background Message Title";
+  const notificationTitle = payload.notification.title;
   const notificationOptions = {
-    body: "Background Message body.",
-    icon: "/firebase-logo.png",
+    body: payload.notification.body,
+    data: payload.data,
+    taskId: payload.data.taskId,
     tag: "notification-1",
   };
 
-  // const channel = new BroadcastChannel("NotificationTaskView");
+  const channel = new BroadcastChannel("NotificationTaskView");
 
-  // //after use click notify on panner Inside the notification click event handler
-  // self.registration.getNotifications().then(function (notifications) {
-  //   // console.log("channel.postMessage ", notifications);
-  //   // const dataSent = [];
-  //   // notifications.forEach((element) => {
-  //   //   if (element.data.FCM_MSG) {
-  //   //     let newObject = {
-  //   //       data: element.data.FCM_MSG,
-  //   //     };
-  //   //     dataSent.push(newObject);
-  //   //   }
-  //   // });
-  //   // channel.postMessage(dataSent);
-  // });
+  //after use click notify on panner Inside the notification click event handler
+  self.registration.getNotifications().then(function (notifications) {
+    console.log("channel.postMessage ", notifications);
+    const dataSent = [];
+    notifications.forEach((element) => {
+      if (element.data.FCM_MSG) {
+        let newObject = {
+          data: element.data.FCM_MSG,
+        };
+        dataSent.push(newObject);
+      }
+    });
 
-  // self.registration.hideNotification();
-  self.registration.showNotification(notificationTitle, notificationOptions);
+    channel.postMessage(dataSent);
+  });
+
+  //self.registration.hideNotification();
+
+  //self.registration.showNotification(notificationTitle, notificationOptions);
 });
